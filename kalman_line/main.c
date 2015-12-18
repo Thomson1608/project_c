77bcc6eb6ddd2f7fc603f050c3197525f503ae10
minht_000 <minht_000@minhtan>
2015-12-18 11:06:02 +0700

    kalman robocon lien(18/12/2015)

---
+++ kalman_line/main.c
@@ -0,0 +1,51 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 9, 2015
+ *      Author: minht_000
+ */
+
+#include <stdio.h>
+
+float Q = 0.100;
+float R = 20;
+float K;
+float P;
+float P_temp;
+float x_temp_est;
+float x_est;
+float z_measured[20]={70,600,100,80,90,500,460,255,0,230,60,45,90,450,560,670,780,0,0,0}; //the 'noisy' value we measured
+float z_real = 0; //the ideal value we wish to measure
+float sum_error_kalman = 0;
+float sum_error_measure = 0;
+float x_est_last = 0;
+float P_last = 0;
+int main(void)
+{
+	int i=0;
+	double LightValue=0;
+
+
+
+
+	for(i=0;i<20;i++)
+	{
+		x_temp_est = x_est_last;
+		P_temp = P_last + Q;
+		//calculate the Kalman gain
+		K = P_temp * ((float)(1.0/(P_temp + R)));
+		//measure
+//		z_measured = (float)((avg) / (sum));
+
+		LightValue = x_temp_est + K * (z_measured[i] - x_temp_est);
+		printf("kq :%f\n",LightValue);
+		P = (1- K) * P_temp;
+		P_last = P;
+
+		//LightValue= (long) LightValue;
+		x_est_last = LightValue;
+	}
+
+	return 0;
+}
+
